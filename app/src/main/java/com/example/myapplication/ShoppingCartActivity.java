c6d1b8e9a06472a52845e87fe08b35207083f99d
2021061-kdu <119ggman@naver.com>
2025-06-10 20:41:58 +0900

    영수증 스캔시 이미지 포함, 기본 아이콘 변경

--- app/src/main/java/com/example/myapplication/ShoppingCartActivity.java
+++ app/src/main/java/com/example/myapplication/ShoppingCartActivity.java
@@ -123,8 +123,8 @@ public class ShoppingCartActivity extends AppCompatActivity {
         View dialogView = getLayoutInflater().inflate(R.layout.dialog_add_item, null);
 
         AutoCompleteTextView etName = dialogView.findViewById(R.id.et_item_name);
-        Button btnDecrease = dialogView.findViewById(R.id.btn_decrease);
-        Button btnIncrease = dialogView.findViewById(R.id.btn_increase);
+        ImageButton btnDecrease = dialogView.findViewById(R.id.btn_decrease);
+        ImageButton  btnIncrease = dialogView.findViewById(R.id.btn_increase);
         TextView tvQuantity = dialogView.findViewById(R.id.tv_quantity);
         Spinner spinnerUnit = dialogView.findViewById(R.id.spinner_unit);
 
